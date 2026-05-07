8ad0f3dc317b994e7cf671e6f293dd597755ce64
MoukthikaT <moukthikatataverthy@gmail.com>
2026-05-07 10:32:28 +0530

    Updated Backend Codes

--- blog-app/Backend/APIs/UserAPI.js
+++ blog-app/Backend/APIs/UserAPI.js
@@ -1,7 +1,9 @@
 import exp from "express";
 import { verifyToken } from "../middlewares/VerifyToken.js";
 import { ArticleModel } from "../Models/ArticleModel.js";
+
 export const userApp = exp.Router();
+
 
 //Read articles of all authors
 userApp.get("/articles", verifyToken("USER"), async (req, res) => {
@@ -9,22 +11,64 @@ userApp.get("/articles", verifyToken("USER"), async (req, res) => {
   const articlesList = await ArticleModel.find({ isArticleActive: true });
   //send res
   res.status(200).json({ message: "articles", payload: articlesList });
+// Read articles of all authors
+userApp.get("/articles", verifyToken("USER"), async (req, res, next) => {
+  try {
+    // read articles
+    const articlesList = await ArticleModel.find({
+      isArticleActive: true,
+    });
+
+    // send response
+    res.status(200).json({
+      message: "articles fetched successfully",
+      payload: articlesList,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
-//Add comment to an article
-userApp.put("/articles", verifyToken("USER"), async (req, res) => {
-  //get body from req
-  const { articleId, comment } = req.body;
-  //check article
-  const articleDocument = await ArticleModel
-                          .findOne({ _id: articleId, isArticleActive: true })
-                           .populate("comments.user");
+// Add comment to an article
+userApp.put("/articles", verifyToken("USER"), async (req, res, next) => {
+  try {
+    // get body from request
+    const { articleId, comment } = req.body;
 
-  console.log(articleDocument);
-  //if article nbot found
-  if (!articleDocument) {
-    return res.status(404).json({ message: "Article not found" });
+    // check article
+    const articleDocument = await ArticleModel.findOne({
+      _id: articleId,
+      isArticleActive: true,
+    }).populate("comments.user");
+
+    // if article not found
+    if (!articleDocument) {
+      return res.status(404).json({
+        message: "Article not found",
+      });
+    }
+
+    // get user id
+    const userId = req.user?.id;
+
+    // add comment
+    articleDocument.comments.push({
+      user: userId,
+      comment,
+    });
+
+    // save article
+    await articleDocument.save();
+
+    // send response
+    res.status(200).json({
+      message: "Comment added successfully",
+      payload: articleDocument,
+    });
+  } catch (err) {
+    next(err);
   }
+
   //get user id
   const userId = req.user?.id;
   //add comment to comments array of articleDocument
@@ -33,4 +77,5 @@ userApp.put("/articles", verifyToken("USER"), async (req, res) => {
   await articleDocument.save();
   //send res
   res.status(200).json({ message: "Comment added successfully", payload: articleDocument });
+});
 });
